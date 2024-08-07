0694dc1b6d0e1dade89f819dd8ffff644cb14571
Ahmed <120693536+EasyCS50@users.noreply.github.com>
2024-08-07 18:37:21 -0400

    Completed 3.8

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
+
+morgan.token('type', (request, response) => {
+  return JSON.stringify(request.body)
+})
 
 let persons = [
   { 
